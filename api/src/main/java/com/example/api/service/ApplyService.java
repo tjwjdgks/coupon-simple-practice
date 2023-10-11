73266daeb3c87174feb138a61c94a79e6557651a
tjwjdgks <tjwjdgks43@gmail.com>
2023-10-11 20:45:39 +0900

    feat: using kafka

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.producer.CouponCreateProducer;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,12 @@ public class ApplyService {
 
     private final CouponCountRepository couponCountRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository) {
+    private final CouponCreateProducer couponCreateProducer;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
     }
 
     public void apply(Long userId) {
@@ -23,7 +27,6 @@ public class ApplyService {
         if(count > 100){
             return;
         }
-
-        couponRepository.save(new Coupon(userId));
+        couponCreateProducer.create(userId);
     }
 }
